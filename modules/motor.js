9ef6e7785c03215a91c087411cb02a82554ab1be
Amiruddin Irsyad <amiruddinid@gmail.com>
2022-10-14 17:47:48 +0700

    create + update

--- modules/motor.js
+++ modules/motor.js
@@ -50,20 +50,28 @@ class Motor{
     handleCreateMotor(req, res){
         // 'INSERT INTO (nama, manufaktur, tgl_pembuatan) VALUES (?,?,?)'
         // [req.body.nama, req.body.manufaktur, req.body.tgl_pembuatan]
-        const motors = this.#motors
-        req.body.id = motors[motors.length - 1].id + 1;
-        this.#motors.push(req.body);
-        res.send('Data Ditambahkan!')
+        db.query(
+            `INSERT INTO motor (nama, transmisi, manufaktur, tgl_pembuatan, foto, harga_sewa, created_by) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)`,
+            [req.body.nama, req.body.transmisi, req.body.manufaktur, req.body.tgl_pembuatan, req.body.foto, req.body.harga_sewa, 'admin'],
+            function(err, results, fields) {
+              console.log(err, results); // results contains rows returned by server
+              res.send('Data Ditambahkan!')
+            }
+        );
     }
 
     // karena menggunakan method selain GET harus di coba di postman
     handleUpdateMotor(req, res){
         // menggunakan req.params.id & req.body
-        console.log(req.params.id, req.body)
-        const index = this.#motors.findIndex(el => el.id == req.params.id);  // menemukan index dari data motor berdasarkan id contoh ada di handleDeleteMotor
-        req.body.id = req.params.id; // karena body tidak mempunyai id, tambahkan id yg didapat dari parameter id / req.params.id
-        this.#motors[index] = req.body;  // gunakan index tersebut untuk mereplace data this.#motors by id dengan req.body
-        res.send('Data Di update dengan id' + req.params.id);
+        db.query(
+            `UPDATE motor SET nama=?, transmisi=?, manufaktur=?, tgl_pembuatan=?, foto=?, harga_sewa=?, updated_by=?`,
+            [req.body.nama, req.body.transmisi, req.body.manufaktur, req.body.tgl_pembuatan, req.body.foto, req.body.harga_sewa, 'admin'],
+            function(err, results, fields) {
+              console.log(results); // results contains rows returned by server
+              res.send('Data Updated!')
+            }
+        );
     }
 
     // karena menggunakan method selain GET harus di coba di postman
